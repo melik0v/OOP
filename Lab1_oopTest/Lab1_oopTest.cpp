f2fcc2e62a020135f38b7a4f55690a8907211578
melik0v <rmelik00v@gmail.com>
2022-04-01 23:05:20 +0700

    Added test for consonants

---
+++ Lab1_oopTest/Lab1_oopTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab1oopTest
+{
+	TEST_CLASS(Lab1oopTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
